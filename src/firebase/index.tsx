d5cdbf8674efc57b2c7d9fac444ad2d6e62ec57a
firth <fm@goupscale.com>
2024-05-12 23:40:40 -0700

    update-firebase

--- src/firebase/index.tsx
+++ src/firebase/index.tsx
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBEKykpHoLR_DiTIQ-OHcwM0Jv1YOVsPpA",
-  authDomain: "dress-rehearsal-store.firebaseapp.com",
-  projectId: "dress-rehearsal-store",
-  storageBucket: "dress-rehearsal-store.appspot.com",
-  messagingSenderId: "264046242112",
-  appId: "1:264046242112:web:b4cfb2a143d51adc1fb2a5",
-  measurementId: "G-CT1XY2Y1BH"
+  apiKey: "AIzaSyAFAMTsxj_IfFxmtVI5fUicE5aIdPCq_Qs",
+  authDomain: "dressrehearsal-fe.firebaseapp.com",
+  projectId: "dressrehearsal-fe",
+  storageBucket: "dressrehearsal-fe.appspot.com",
+  messagingSenderId: "537614160571",
+  appId: "1:537614160571:web:881bf08ae62db14a304f9e",
+  measurementId: "G-WTB8H5MMTC",
 };
 
 // Initialize Firebase
